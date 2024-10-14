c6d521962fa0f465d0567bd6a9873bafa5e68953
Oleksiy Bondar <bondaroleksiyandriyovich@gmail.com>
2024-10-14 16:51:12 +0200

    - fix CI problem

--- src/hyperiontf/api/rest_client/client.py
+++ src/hyperiontf/api/rest_client/client.py
@@ -55,12 +55,12 @@ class Client:
         :param connection_timeout: The timeout for establishing a connection.
         :param request_timeout: The timeout for completing a request.
         """
-        self.scheme: str
-        self.netloc: str
-        self.path: str
-        self.params: str
-        self.query: str
-        self.fragment: str
+        self.scheme: Optional[str]
+        self.netloc: Optional[str]
+        self.path: Optional[str]
+        self.params: Optional[str]
+        self.query: Optional[str]
+        self.fragment: Optional[str]
 
         self.scheme, self.netloc, self.path, self.params, self.query, self.fragment = (
             self._parse_url(url)
