a99887b9b8a119b0251d49346872130496162589
Oleksiy Bondar <bondaroleksiyandriyovich@gmail.com>
2024-10-16 13:59:00 +0200

    - export TestReporter

--- src/hyperiontf/executors/pytest/__init__.py
+++ src/hyperiontf/executors/pytest/__init__.py
@@ -1,5 +1,6 @@
 from .fixture import fixture
 from .logger_setup import automatic_log_setup
+from .helper.test_reporter import TestReporter
 
 
-__all__ = ["automatic_log_setup", "fixture"]
+__all__ = ["automatic_log_setup", "fixture", "TestReporter"]
