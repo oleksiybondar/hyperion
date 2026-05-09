c333d8ad3f6d678e4e6e9917f3ed040128a543ce
Oleksiy Bondar <bondaroleksiyandriyovich@gmail.com>
2026-05-09 22:30:34 +0200

    update timeout for webkit

--- src/hyperiontf/ui/adapters/playwright/element.py
+++ src/hyperiontf/ui/adapters/playwright/element.py
@@ -349,7 +349,7 @@ class Element:
         """
         try:
             # Try real user click first (fail fast)
-            self.element.click(timeout=3000)
+            self.element.click(timeout=self.click_timeout)
         except TimeoutError as e:
             if "element is not visible" in e.message:
                 tag = self.element.evaluate("element => element.tagName.toLowerCase()")
@@ -358,6 +358,12 @@ class Element:
                     return self.element.evaluate(NATIVE_SELECT_HEADLESS_WORKAROUND)
 
             raise e
+
+    @property
+    def click_timeout(self):
+        if self.page.browser == "webkit":
+            return 5000
+        return 3000
 
     @map_exception
     @assert_stale_reference
